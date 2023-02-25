f9791b66adc3f6ed9415900b122b0dc574a34343
unknown <j.kurtbayaras@gmail.com>
2023-02-25 20:48:44 +0800

    feature: add spinner

---
+++ src/components/CustomSpinner.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="custom-spinner">
+    <div class="spinner-circle"></div>
+  </div>
+</template>
+
+<style>
+.custom-spinner {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+}
+
+.spinner-circle {
+  width: 20px;
+  height: 20px;
+  border: 2px solid #67C23A;
+  border-top-color: transparent;
+  border-radius: 50%;
+  animation: spin 1s linear infinite;
+}
+
+@keyframes spin {
+  from { transform: rotate(0deg); }
+  to { transform: rotate(360deg); }
+}
+</style>

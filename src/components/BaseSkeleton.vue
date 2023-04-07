0bb72b04f9bc28722a3b33ae13d71cba30316244
kurt2 <j.kurtbayaras@gmail.com>
2023-04-07 21:15:24 +0800

    refactor: add skeleton on updates

--- src/components/BaseSkeleton.vue
+++ src/components/BaseSkeleton.vue
@@ -1,53 +1,112 @@
 <template>
-  <div class="skeleton">
-    <el-skeleton style="width: 130px" animated>
-      <template #template>
-        <el-skeleton-item variant="image" class="skeleton-img" />
-      </template>
-    </el-skeleton>
-    <el-skeleton class="skeleton-2">
-      <template #template>
-        <el-skeleton-item variant="rect" style="width: 30%" />
-        <el-skeleton-item variant="rect" style="width: 50%" />
-        <el-skeleton-item variant="rect" style="width: 90%" />
-        <el-skeleton-item variant="rect" />
-      </template>
-    </el-skeleton>
+  <div v-if="cardLong">
+    <div class="skeleton">
+      <el-skeleton style="width: 130px" animated>
+        <template #template>
+          <el-skeleton-item variant="image" class="skeleton-img" />
+        </template>
+      </el-skeleton>
+      <el-skeleton class="skeleton-2">
+        <template #template>
+          <el-skeleton-item variant="rect" style="width: 30%" />
+          <el-skeleton-item variant="rect" style="width: 50%" />
+          <el-skeleton-item variant="rect" style="width: 90%" />
+          <el-skeleton-item variant="rect" />
+        </template>
+      </el-skeleton>
+    </div>
+    <div class="skeleton">
+      <el-skeleton style="width: 130px" animated>
+        <template #template>
+          <el-skeleton-item variant="image" class="skeleton-img" />
+        </template>
+      </el-skeleton>
+      <el-skeleton class="skeleton-2">
+        <template #template>
+          <el-skeleton-item variant="rect" style="width: 30%" />
+          <el-skeleton-item variant="rect" style="width: 50%" />
+          <el-skeleton-item variant="rect" style="width: 90%" />
+          <el-skeleton-item variant="rect" />
+        </template>
+      </el-skeleton>
+    </div>
+    <div class="skeleton">
+      <el-skeleton style="width: 130px" animated>
+        <template #template>
+          <el-skeleton-item variant="image" class="skeleton-img" />
+        </template>
+      </el-skeleton>
+      <el-skeleton class="skeleton-2">
+        <template #template>
+          <el-skeleton-item variant="rect" style="width: 30%" />
+          <el-skeleton-item variant="rect" style="width: 50%" />
+          <el-skeleton-item variant="rect" style="width: 90%" />
+          <el-skeleton-item variant="rect" />
+        </template>
+      </el-skeleton>
+    </div>
   </div>
-  <div class="skeleton">
-    <el-skeleton style="width: 130px" animated>
-      <template #template>
-        <el-skeleton-item variant="image" class="skeleton-img" />
-      </template>
-    </el-skeleton>
-    <el-skeleton class="skeleton-2">
-      <template #template>
-        <el-skeleton-item variant="rect" style="width: 30%" />
-        <el-skeleton-item variant="rect" style="width: 50%" />
-        <el-skeleton-item variant="rect" style="width: 90%" />
-        <el-skeleton-item variant="rect" />
-      </template>
-    </el-skeleton>
-  </div>
-  <div class="skeleton">
-    <el-skeleton style="width: 130px" animated>
-      <template #template>
-        <el-skeleton-item variant="image" class="skeleton-img" />
-      </template>
-    </el-skeleton>
-    <el-skeleton class="skeleton-2">
-      <template #template>
-        <el-skeleton-item variant="rect" style="width: 30%" />
-        <el-skeleton-item variant="rect" style="width: 50%" />
-        <el-skeleton-item variant="rect" style="width: 90%" />
-        <el-skeleton-item variant="rect" />
-      </template>
-    </el-skeleton>
+  <div v-else>
+    <div class="no-border">
+      <el-skeleton style="width: 130px" animated>
+        <template #template>
+          <el-skeleton-item variant="image" class="noborder-img"/>
+        </template>
+      </el-skeleton>
+      <el-skeleton class="no-border-2">
+        <template #template>
+          <el-skeleton-item variant="rect" style="width: 30%" />
+          <el-skeleton-item variant="rect" style="width: 50%" />
+          <el-skeleton-item variant="rect" style="width: 90%" />
+          <el-skeleton-item variant="rect" />
+        </template>
+      </el-skeleton>
+      <el-skeleton style="width: 130px" animated>
+        <template #template>
+          <el-skeleton-item variant="image" class="noborder-img"/>
+        </template>
+      </el-skeleton>
+      <el-skeleton class="no-border-2">
+        <template #template>
+          <el-skeleton-item variant="rect" style="width: 30%" />
+          <el-skeleton-item variant="rect" style="width: 50%" />
+          <el-skeleton-item variant="rect" style="width: 90%" />
+          <el-skeleton-item variant="rect" />
+        </template>
+      </el-skeleton>
+      <el-skeleton style="width: 130px" animated>
+        <template #template>
+          <el-skeleton-item variant="image" class="noborder-img"/>
+        </template>
+      </el-skeleton>
+      <el-skeleton class="no-border-2">
+        <template #template>
+          <el-skeleton-item variant="rect" style="width: 30%" />
+          <el-skeleton-item variant="rect" style="width: 50%" />
+          <el-skeleton-item variant="rect" style="width: 90%" />
+          <el-skeleton-item variant="rect" />
+        </template>
+      </el-skeleton>
+      <el-skeleton style="width: 130px" animated>
+        <template #template>
+          <el-skeleton-item variant="image" class="noborder-img"/>
+        </template>
+      </el-skeleton>
+      <el-skeleton class="no-border-2">
+        <template #template>
+          <el-skeleton-item variant="rect" style="width: 30%" />
+          <el-skeleton-item variant="rect" style="width: 50%" />
+          <el-skeleton-item variant="rect" style="width: 90%" />
+          <el-skeleton-item variant="rect" />
+        </template>
+      </el-skeleton>
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
 import { ref } from "vue";
+defineProps(["cardLong"])
 const items = ref([1, 2, 3]);
 </script>
 
@@ -59,6 +118,12 @@ const items = ref([1, 2, 3]);
   display: flex;
   padding: 10px 10px;
   margin-bottom: 10px;
+}
+
+.no-border {
+  display: grid;
+  grid-template-columns: .5fr 2fr;
+  gap: 20px;
 }
 
 .skeleton-img {
@@ -73,5 +138,22 @@ const items = ref([1, 2, 3]);
   display: flex;
   flex-direction: column;
   justify-content: space-evenly;
+}
+
+.no-border-2 {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  position: relative;
+  right: 40px;
+}
+
+.noborder-img {
+  padding: 10px;
+  width: 100px;
+  height: 100px;
+  border-radius: 15px;
+  display: flex;
+  height: 6em;
 }
 </style>
--- src/views/AppDashboard.vue
+++ src/views/AppDashboard.vue
@@ -96,7 +96,7 @@
           </div>
           <el-row v-else>
             <el-col :span="24" class="skeleton-load">
-              <BaseSkeleton />
+              <BaseSkeleton :cardLong="true" />
             </el-col>
           </el-row>
         </el-col>
@@ -122,27 +122,32 @@
       <div>
         <h2>Recent Updates</h2>
         <el-scrollbar height="50vh">
-          <div
-            v-for="task in allTasks"
-            :key="task._id"
-            class="task-updates"
-          > 
-            <div class="recent-updates">
-              <div  v-if="task.type === 'Today'" class="task-today-icon">
-                <img src="@/assets/sun.svg" class="" />
-              </div>
-              <div class="task-personal-icon" v-else-if="(task.type === 'Personal')">
-                  <img src="../assets/boy.svg" />
-              </div>
-              <div class="task-work-icon" v-else>
-                  <img src="../assets/work.svg" />
-              </div>
-
-              <div class="recent-title">
-                <h3>{{ task.title }}</h3> 
-                <span :class="[task.completed ? 'completed': 'ongoing']">{{ task.completed !== true ? "Ongoing" : `Completed`}}</span>
+          <div v-if="allTasks.length > 0 && !status.isLoading">
+            <div
+              v-for="task in allTasks"
+              :key="task._id"
+              class="task-updates"
+            > 
+              <div class="recent-updates">
+                <div  v-if="task.type === 'Today'" class="task-today-icon">
+                  <img src="@/assets/sun.svg" class="" />
+                </div>
+                <div class="task-personal-icon" v-else-if="(task.type === 'Personal')">
+                    <img src="../assets/boy.svg" />
+                </div>
+                <div class="task-work-icon" v-else>
+                    <img src="../assets/work.svg" />
+                </div>
+  
+                <div class="recent-title">
+                  <h3>{{ task.title }}</h3> 
+                  <span :class="[task.completed ? 'completed': 'ongoing']">{{ task.completed !== true ? "Ongoing" : `Completed`}}</span>
+                </div>
               </div>
             </div>
+          </div>
+          <div v-else>
+            <BaseSkeleton />
           </div>
         </el-scrollbar>
       </div>
